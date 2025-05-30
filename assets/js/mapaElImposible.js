b8035b2f97d0d672ff93455da9a86fd3d19b2a35
Alexander Martinez <miltonherrera110@gmail.com>
2025-05-29 20:47:40 -0600

    Diseño copiar y pegar

--- assets/js/mapaElImposible.js
+++ assets/js/mapaElImposible.js
@@ -13,3 +13,18 @@ window.initMap = function () {
     title: "Estamos aquí",
   });
 };
+
+window.initMap = function () {
+  const ubicacion = { lat: 13.902625748654533, lng: -89.8190279137476 };
+
+  const map = new google.maps.Map(document.getElementById("mapTermales"), {
+    zoom: 15,
+    center: ubicacion,
+  });
+
+  const marker = new google.maps.Marker({
+    position: ubicacion,
+    map: map,
+    title: "Estamos aquí",
+  });
+};

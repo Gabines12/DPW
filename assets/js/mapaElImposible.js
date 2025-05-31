8a3d0a9eb39acf58fd958d2a060ab46d97f9a700
Alexander Martinez <miltonherrera110@gmail.com>
2025-05-30 19:50:15 -0600

    Cambios en el index y anhadi nuevo css

--- assets/js/mapaElImposible.js
+++ assets/js/mapaElImposible.js
@@ -1,30 +1,31 @@
-// Asegura que initMap esté disponible globalmente para Google Maps
 window.initMap = function () {
-  const ubicacion = { lat: 13.833381568986654, lng: -89.93468826243083 };
+  // Para elImposible.html
+  var divImposible = document.getElementById("map");
+  if (divImposible) {
+    const ubicacion = { lat: 13.833381568986654, lng: -89.93468826243083 };
+    const map = new google.maps.Map(divImposible, {
+      zoom: 15,
+      center: ubicacion,
+    });
+    new google.maps.Marker({
+      position: ubicacion,
+      map: map,
+      title: "Parque Nacional El Imposible",
+    });
+  }
 
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
-    center: ubicacion,
-  });
-
-  const marker = new google.maps.Marker({
-    position: ubicacion,
-    map: map,
-    title: "Estamos aquí",
-  });
-};
-
-window.initMap = function () {
-  const ubicacion = { lat: 13.902625748654533, lng: -89.8190279137476 };
-
-  const map = new google.maps.Map(document.getElementById("mapTermales"), {
-    zoom: 15,
-    center: ubicacion,
-  });
-
-  const marker = new google.maps.Marker({
-    position: ubicacion,
-    map: map,
-    title: "Estamos aquí",
-  });
+  // Para Termales.html
+  var divTermales = document.getElementById("mapTermales");
+  if (divTermales) {
+    const ubicacion = { lat: 13.902625748654533, lng: -89.8190279137476 };
+    const map = new google.maps.Map(divTermales, {
+      zoom: 15,
+      center: ubicacion,
+    });
+    new google.maps.Marker({
+      position: ubicacion,
+      map: map,
+      title: "Termales de Ahuachapán",
+    });
+  }
 };
